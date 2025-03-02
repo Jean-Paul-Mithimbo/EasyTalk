013d2a39b196dabf0306b5935fcc570de9d8f4dc
Jp <pyjean15@gmail.com>
2025-03-02 13:17:05 +0200

    selection user sur page d'acceuil et autre

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:easy_talk/Pages/chat_page.dart';
+import 'package:easy_talk/components/user_tile.dart';
 import 'package:easy_talk/services/auth/auth_service.dart';
 import 'package:easy_talk/components/my_drawer.dart';
 import 'package:easy_talk/services/chat/chat_service.dart';
@@ -20,25 +22,47 @@ class HomePage extends StatelessWidget {
       drawer: const MyDrawer(),
       body: _buildUserList(),
     );
+  }
 
-    // build a list of users for the current logged in user
-    Widget _buildUserList() {
-      return StreamBuilder( 
-          stream: _chatService.getUsersStream(),
-          builder: (context, snapshop) {
-            // error 
-            if(snapshop.hasErrror){
-              return const Text('Error');
-            }
-            //loading  
-            if (snapshop.connectionState==ConnectionState.waiting){
-              return const Text('Loading..')
-            }
-            // return List view
-            return ListView(
-              children: snapshop.data!.map<Widget>((userData)=>_buidListItem).toList(),
-            );
-          });
-    }
+  // build a list of users for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context, snapshop) {
+          // error
+          if (snapshop.hasError) {
+            return const Text('Error');
+          }
+          //loading
+          if (snapshop.connectionState == ConnectionState.waiting) {
+            return const Text('Loading..');
+          }
+          // return List view
+          return ListView(
+            children: snapshop.data!
+                .map<Widget>((userData) => _buidUserListItem(userData,context))
+                .toList(),
+          );
+        });
+  }
+
+  // build individual list tile for user
+  Widget _buidUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    // display all users except current user
+    return UserTile(
+      text: userData["email"],
+      onTap: () {
+        // tapped on a user -> go to chat page
+
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverEmail: userData["email"],
+              ),
+            ));
+      },
+    );
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,15 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  // instance of auth
+  // instance of auth & firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  final FirebaseFirestore _firestore=FirebaseFirestore.instance;
   //Sign in ou login
   Future<UserCredential> SignInWithEmailPassword(String email, password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        // sign user in
         email: email,
         password: password,
+      );
+       // save user info if it doesn'exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid':userCredential.user!.uid,
+          'email':email,
+        },
       );
       return userCredential;
     } on FirebaseAuthException catch (e) {
@@ -20,10 +29,19 @@ class AuthService {
   //Sign up
  Future<UserCredential> SignUpWithEmailPassword(String email, password) async {
     try {
+      // create user
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+      // save user info in a separate doc 
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid':userCredential.user!.uid,
+          'email':email,
+        },
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
